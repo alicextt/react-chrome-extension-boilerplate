ce3f50ad463a7e073ef84a7a915d561bef25a02e
txu <txu@godaddy.com>
2018-02-01 23:02:22 -0800

    all commands

--- app/components/MainSection.js
+++ app/components/MainSection.js
@@ -59,19 +59,24 @@ export default class MainSection extends Component {
   renderSearch() {
     const { input } = this.state;
     // console.log(dictKey);
-    var scores = dictKey.map((v) => {
-      return [scoreFunc(v, input|| '', 0), v];
-    }) || [];
     var res = [];
+    var scores = [];
+    if(input === '*'){
+      dictKey.sort().map((word) => {
+          res.push(<a className={style.button} key={word} onClick={ this.sendAction.bind(null, word) }> {word}</a>);
+      });
+    } else {
+      scores = dictKey.map((v) => {
+        return [scoreFunc(v, input|| '', 0), v];
+      }) || [];
+      scores.sort(function(a, b){ return a[0]-b[0] }).map((props) => {
+        if(props[0]>=0.3){
+          const word = props[1];
+          res.push(<a className={style.button} key={word} onClick={ this.sendAction.bind(null, word) }> {word}</a>);
+        }
+      });
+    }
 
-    scores.sort(function(a, b){ return a[0]-b[0] });
-
-    scores.map((props) => {
-      if(props[0]>=0.3){
-        const word = props[1];
-        res.push(<a className={style.button} key={word} onClick={ this.sendAction.bind(null, word) }> {word}</a>);
-      }
-    });
     return (
       <div>{res}</div>
     );
@@ -97,7 +102,7 @@ export default class MainSection extends Component {
       <section className={style.main}>
       <div className={style.searchbox}>
         <svg className= { style.magnify} viewBox="0 0 48 48"><path d="M31 28h-1.59l-.55-.55C30.82 25.18 32 22.23 32 19c0-7.18-5.82-13-13-13S6 11.82 6 19s5.82 13 13 13c3.23 0 6.18-1.18 8.45-3.13l.55.55V31l10 9.98L40.98 38 31 28zm-12 0c-4.97 0-9-4.03-9-9s4.03-9 9-9 9 4.03 9 9-4.03 9-9 9z"/></svg>
-        <input type="text" placeholder="Search here..." onChange={ this.handleChange }/>
+        <input type="text" placeholder="Try add ...." onChange={ this.handleChange }/>
       </div>
       <div>
       { this.renderSearch()}
